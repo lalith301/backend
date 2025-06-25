3e1ba2846780178ca1bd35487afdd912c910691b
lalith301 <lkmm334@gmail.com>
2025-06-25 09:51:18 +0530

    Initial backend commit

--- routes/movies.js
+++ routes/movies.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const Movie = require('../models/Movie');
 const router = express.Router();
 
+// ✅ Auth Middleware
 const auth = (req, res, next) => {
   const token = req.headers.authorization?.split(' ')[1];
-  if (!token) return res.status(403).json({ message: 'No token' });
+  if (!token) return res.status(403).json({ message: 'No token provided' });
   try {
     req.user = jwt.verify(token, process.env.JWT_SECRET);
     next();
@@ -14,15 +14,22 @@ const auth = (req, res, next) => {
   }
 };
 
-router.get('/', auth, async (req, res) => {
-  const movies = await Movie.find({ author: req.user.id });
-  res.json(movies);
-});
-
-router.post('/', auth, async (req, res) => {
-  const movie = new Movie({ ...req.body, author: req.user.id });
-  await movie.save();
-  res.json(movie);
+// ✅ GET /api/movies - return 2 hardcoded movies
+router.get('/', auth, (req, res) => {
+  res.json([
+    {
+      Title: 'Inception',
+      Year: '2010',
+      Type: 'Movie',
+      Poster: 'https://m.media-amazon.com/images/I/51nbVEuw1HL._AC_.jpg'
+    },
+    {
+      Title: 'Interstellar',
+      Year: '2014',
+      Type: 'Movie',
+      Poster: 'https://m.media-amazon.com/images/I/71niXI3lxlL._AC_SY679_.jpg'
+    }
+  ]);
 });
 
 module.exports = router;
